6fb7e1602a7db2927d194c30ec67b41f996a0f6c
Liedson <liedsonwork@hotmail.com>
2025-04-10 17:35:23 -0300

    feat: criação da segunda section

--- src/components/BigCardGlasses/index.tsx
+++ src/components/BigCardGlasses/index.tsx
@@ -20,29 +20,37 @@ export default function BigCardGlasses({
   linkImgEnterprise: string
 }) {
   return (
-    <div className={`bg-white p-5 w-[350px] h-[300px] rounded-[8px] ${poppins.className}`}>
-      <div>
-        <div className="flex justify-center">
-          <Image src={linkImgGlasses} alt={title} width={270} height={270} className="w-[270px]" />
+    <div className={`bg-white p-5 w-[350px] h-[300px] rounded-[8px] flex flex-col ${poppins.className}`}>
+      <div className="flex-1">
+        <div className="flex justify-center items-center h-32">
+          <div className="relative w-[270px] h-[130px]">
+            <Image
+              src={linkImgGlasses}
+              alt={title}
+              fill
+              className="object-contain"
+            />
+          </div>
         </div>
-        <div className="flex justify-end mt-7 mb-2">
+        <div className="flex justify-end mt-4 mb-1">
           <span className="text-[14px] text-black tracking-[0.09em] font-[300]">IMAGEM MERAMENTE ILUSTRATIVA</span>
         </div>
         <div className="w-full h-[1px] bg-black my-2"></div>
       </div>
-      <div className="mt-6">
+      <div className="mt-4">
         <h3 className="text-black tracking-[0.1em] font-[400]">MODELO: {modelo}</h3>
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center mt-2">
           <h3 className="text-[14px] text-black self-end tracking-[0.09em] font-[300]">
             COLEÇÃO <span className="font-bold">{collection}</span>
           </h3>
-          <Image
-            src={linkImgEnterprise}
-            alt={title}
-            width={120}
-            height={120}
-            className="w-[120px]"
-          />
+          <div className="relative w-[120px] h-[25px]">
+            <Image
+              src={linkImgEnterprise}
+              alt={title}
+              fill
+              className="object-contain"
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Section2/index.tsx
+++ src/components/Section2/index.tsx
@@ -1,40 +1,148 @@
-import BigCardGlasses from '../BigCardGlasses'
 import SmallCardGlasses from '../SmallCardGlasses'
+import Image from 'next/image'
+
+const mormaiGlasses = [
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/mormai-agra.svg",
+    modelo: "M6117A1451",
+    collection: "AGRA",
+    linkImgEnterprise: "/img/Enterprises/mormaii.svg"
+  },
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/mormai-ancara2.svg",
+    modelo: "M6142DH95",
+    collection: "ANCARA 2",
+    linkImgEnterprise: "/img/Enterprises/mormaii.svg"
+  },
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/mormai-angel.svg",
+    modelo: "M6079AA35",
+    collection: "ANGEL",
+    linkImgEnterprise: "/img/Enterprises/mormaii.svg"
+  },
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/mormai-asana.svg",
+    modelo: "M6053A1452",
+    collection: "ASANA",
+    linkImgEnterprise: "/img/Enterprises/mormaii.svg"
+  },
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/mormai-hover.svg",
+    modelo: "M6160A8558",
+    collection: "HOVER",
+    linkImgEnterprise: "/img/Enterprises/mormaii.svg"
+  },
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/colcci-gabi.svg",
+    modelo: "0012ASDASD123AS",
+    collection: "GABI",
+    linkImgEnterprise: "/img/Enterprises/mormaii.svg"
+  }
+];
+
+const jeanPierreGlasses = [
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/mormai-agra.svg",
+    modelo: "M6117A1451",
+    collection: "AGRA",
+    linkImgEnterprise: "/img/Enterprises/jean-pierre.svg"
+  },
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/colcci-gabi.svg",
+    modelo: "0012ASDASD123AS",
+    collection: "GABI",
+    linkImgEnterprise: "/img/Enterprises/jean-pierre.svg"
+  },
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/colcci-gabi.svg",
+    modelo: "0012ASDASD123AS",
+    collection: "GABI",
+    linkImgEnterprise: "/img/Enterprises/jean-pierre.svg"
+  },
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/colcci-gabi.svg",
+    modelo: "0012ASDASD123AS",
+    collection: "GABI",
+    linkImgEnterprise: "/img/Enterprises/jean-pierre.svg"
+  },
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/colcci-gabi.svg",
+    modelo: "0012ASDASD123AS",
+    collection: "GABI",
+    linkImgEnterprise: "/img/Enterprises/jean-pierre.svg"
+  },
+  {
+    title: "Óculos de Sol",
+    linkImgGlasses: "/img/GlassesSection2/colcci-gabi.svg",
+    modelo: "0012ASDASD123AS",
+    collection: "GABI",
+    linkImgEnterprise: "/img/Enterprises/jean-pierre.svg"
+  }
+];
 
 export default function Section2() {
   return (
-    <section className="w-full py-20 bg-gray-900 text-white">
-      <div className="container mx-auto px-4 max-w-5xl">
-        <h2 className="text-4xl md:text-5xl font-bold mb-6 text-center">Exemplo de Seção 2</h2>
-        <p className="text-lg mb-6 text-gray-300 text-center max-w-3xl mx-auto">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam commodi
-          minus optio pariatur vitae eos rerum aspernatur error blanditiis, est
-          obcaecati facilis quam corrupti asperiores perferendis impedit!
-        </p>
-        <p className="text-base mb-10 text-gray-300 text-center max-w-3xl mx-auto">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint quisquam
-          commodi ipsa eveniet aspernatur unde ducimus vero distinctio debitis vitae.
-        </p>
-        <div className="flex flex-wrap justify-center items-stretch gap-4 mb-10">
-          <BigCardGlasses
-            title="Óculos de Sol"
-            linkImgGlasses="/img/GlassesSection2/lovarato-duelii.svg"
-            modelo="0012/1234"
-            collection="ÉLLE"
-            linkImgEnterprise="/img/Enterprises/lovarato.svg"
-          />
-           <SmallCardGlasses
-            title="Óculos de Sol"
-            linkImgGlasses="/img/GlassesSection2/colcci-gabi.svg"
-            modelo="0012ASDASD123AS"
-            collection="GABI"
-            linkImgEnterprise="/img/Enterprises/colcci.svg"
+    <section className="w-full bg-white text-black my-10">
+      <div className="flex flex-col lg:flex-row items-center gap-10 mb-20">
+        <div className="w-full lg:w-1/2">
+          <Image
+            src="/img/ImgsSection3/first-image.svg"
+            alt="Section 2"
+            width={840}
+            height={440}
+            className="w-full h-auto"
           />
         </div>
-        <div className="flex justify-center">
-          <button className="px-6 py-3 bg-red-500 hover:bg-red-600 text-white rounded-md transition-colors shadow-md">
-            Ver Todos os Produtos
-          </button>
+        <div className="mt-6 lg:mt-0 flex justify-center">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {mormaiGlasses.map((glass, index) => (
+              <SmallCardGlasses
+                key={index}
+                title={glass.title}
+                linkImgGlasses={glass.linkImgGlasses}
+                modelo={glass.modelo}
+                collection={glass.collection}
+                linkImgEnterprise={glass.linkImgEnterprise}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col-reverse lg:flex-row items-center gap-6 lg:gap-10">
+        <div className="mt-6 lg:mt-0 lg:ml-10 flex justify-center">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {jeanPierreGlasses.map((glass, index) => (
+              <SmallCardGlasses
+                key={index}
+                title={glass.title}
+                linkImgGlasses={glass.linkImgGlasses}
+                modelo={glass.modelo}
+                collection={glass.collection}
+                linkImgEnterprise={glass.linkImgEnterprise}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="w-full lg:w-auto lg:w-1/2">
+          <Image
+            src="/img/ImgsSection3/second-image.svg"
+            alt="Section 2"
+            width={840}
+            height={440}
+            className="w-full h-auto"
+          />
         </div>
       </div>
     </section>
